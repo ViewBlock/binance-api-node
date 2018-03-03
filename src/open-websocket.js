3ebc9c127c4595a45726da088755aedeb3ea36a2
Benny Neugebauer <bn@bennyn.de>
2018-03-03 16:51:26 +0100

    Expected linebreaks to be 'LF'

--- src/open-websocket.js
+++ src/open-websocket.js
@@ -3,16 +3,12 @@ import ReconnectingWebSocket from 'reconnecting-websocket'
 import WebSocket from 'ws'
 
 export default wsAddressBuilder => {
-  return new ReconnectingWebSocket(
-    wsAddressBuilder,
-    undefined,
-    {
-      connectionTimeout: 4000,
-      constructor: typeof window !== 'undefined' ? WebSocket : Html5WebSocket,
-      debug: false,
-      maxReconnectionDelay: 10000,
-      maxRetries: Infinity,
-      minReconnectionDelay: 4000,
-    }
-  );
+  return new ReconnectingWebSocket(wsAddressBuilder, undefined, {
+    connectionTimeout: 4000,
+    constructor: typeof window !== 'undefined' ? WebSocket : Html5WebSocket,
+    debug: false,
+    maxReconnectionDelay: 10000,
+    maxRetries: Infinity,
+    minReconnectionDelay: 4000,
+  })
 }
