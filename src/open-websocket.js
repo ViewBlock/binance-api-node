823b111209545ed5585a7c1368d9971b2186570a
Benny Neugebauer <bn@bennyn.de>
2018-03-05 22:47:49 +0100

    removed unnecessary 'ws'

--- src/open-websocket.js
+++ src/open-websocket.js
@@ -1,9 +1,8 @@
 import Html5WebSocket from 'html5-websocket'
 import ReconnectingWebSocket from 'reconnecting-websocket'
-import WebSocket from 'ws'
 
 export default url => {
-  return new ReconnectingWebSocket(url, undefined, {
+  return new ReconnectingWebSocket(url, null, {
     connectionTimeout: 4E3,
     constructor: typeof window !== 'undefined' ? WebSocket : Html5WebSocket,
     debug: false,
