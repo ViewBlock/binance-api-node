63b45219f8cb815c45999cd83c4a79087caf4e09
maxinminax <ng.nhuphu@gmail.com>
2018-04-04 13:45:44 +0700

    fix issue header Sec-WebSocket-Protocol was null
    https://github.com/HyperCubeProject/binance-api-node/issues/68

--- src/open-websocket.js
+++ src/open-websocket.js
@@ -2,7 +2,7 @@ import Html5WebSocket from 'html5-websocket'
 import ReconnectingWebSocket from 'reconnecting-websocket'
 
 export default url => {
-  return new ReconnectingWebSocket(url, null, {
+  return new ReconnectingWebSocket(url, '', {
     connectionTimeout: 4E3,
     constructor: typeof window !== 'undefined' ? WebSocket : Html5WebSocket,
     debug: false,
