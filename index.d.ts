afbe25fd42ed143542a5d6ded8c025be1f026890
Sebastian N. Fernandez <snf@users.noreply.github.com>
2018-12-28 23:53:05 +0000

    fixing interface for partialDepth (#144)

--- index.d.ts
+++ index.d.ts
@@ -143,7 +143,7 @@ declare module 'binance-api-node' {
 
     export interface WebSocket {
         depth: (pair: string | string[], callback: (depth: Depth) => void) => Function;
-        partialDepth: (options: { symbol: string, level: number }, callback: (depth: PartialDepth) => void) => Function;
+        partialDepth: (options: { symbol: string, level: number } | [{ symbol: string, level: number }], callback: (depth: PartialDepth) => void) => Function;
         ticker: (pair: string, callback: (ticker: Ticker) => void) => Function;
         allTickers: (callback: (tickers: Ticker[]) => void) => Function;
         candles: (pair: string, period: string, callback: (ticker: Candle) => void) => Function;
