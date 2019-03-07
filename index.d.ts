768b2f872a0fbe3c91fffe3c79058981a921e4ba
jester <jestersimpps@gmail.com>
2019-03-07 23:52:26 +0100

    make symbol optional (#163)
    make symbol optional: get all open orders, but keep useServerTime: true

--- index.d.ts
+++ index.d.ts
@@ -168,7 +168,7 @@ declare module 'binance-api-node' {
         myTrades(options: { symbol: string, limit?: number, fromId?: number, useServerTime?: boolean }): Promise<MyTrade[]>;
         getOrder(options: { symbol: string; orderId: number, useServerTime?: boolean }): Promise<QueryOrderResult>;
         cancelOrder(options: { symbol: string; orderId: number, useServerTime?: boolean }): Promise<CancelOrderResult>;
-        openOrders(options: { symbol: string, useServerTime?: boolean }): Promise<QueryOrderResult[]>;
+        openOrders(options: { symbol?: string, useServerTime?: boolean }): Promise<QueryOrderResult[]>;
         dailyStats(options?: { symbol: string }): Promise<DailyStatsResult | DailyStatsResult[]>;
         candles(options: CandlesOptions): Promise<CandleChartResult[]>;
         tradesHistory(options: { symbol: string, limit?: number, fromId?: number }): Promise<Trade[]>;
