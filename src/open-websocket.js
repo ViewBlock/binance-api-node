bca2a7e1617929ec0fda1c9c814f6e994584f7bc
Alexey Marunin <a.v.marunin@gmail.com>
2020-03-03 01:18:42 +0300

    Fix #240: Add 'ping' listener to WS streams

--- src/open-websocket.js
+++ src/open-websocket.js
@@ -2,7 +2,7 @@ import ws from 'ws'
 import ReconnectingWebSocket from 'reconnecting-websocket'
 
 export default url => {
-  return new ReconnectingWebSocket(url, [], {
+  const rws = new ReconnectingWebSocket(url, [], {
     WebSocket: ws,
     connectionTimeout: 4e3,
     debug: false,
@@ -10,4 +10,13 @@ export default url => {
     maxRetries: Infinity,
     minReconnectionDelay: 4e3,
   })
+  // https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md
+  // The websocket server will send a ping frame every 3 minutes. If the websocket server
+  // does not receive a pong frame back from the connection within a 10 minute period,
+  // the connection will be disconnected.
+  const noop = () => undefined
+  const pong = () => rws._ws.pong(noop)
+  const addPingListener = () => rws._ws.on('ping', pong)
+  rws.addEventListener('open', addPingListener)
+  return rws
 }
