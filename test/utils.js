c349610a72cb357a8c2d4fd600d5a4e4383fd10c
Mériadec Pillet <meriadec@users.noreply.github.com>
2022-04-02 17:20:17 +0200

    Add CI action that check lint, prettier & test (#558)

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,4 @@
-import http from 'http';
+import http from 'http'
 
 export const checkFields = (t, object, fields) => {
   fields.forEach(field => {
@@ -7,20 +7,18 @@ export const checkFields = (t, object, fields) => {
 }
 
 const generatePort = (() => {
-  let portNum = 9000;
-  return () => portNum++;
+  let portNum = 9000
+  return () => portNum++
 })()
 
-export const createHttpServer = (requestHandler) => {
-  const server = http.createServer(requestHandler);
-  const port = generatePort();
+export const createHttpServer = requestHandler => {
+  const server = http.createServer(requestHandler)
+  const port = generatePort()
   return {
     url: `http://127.0.0.1:${port}`,
-    start: () => new Promise((resolve, reject) =>
-      server.listen(port, err => err ? reject(err) : resolve())
-    ),
-    stop: () => new Promise((resolve, reject) =>
-      server.close(err => err ? reject(err) : resolve())
-    ),
+    start: () =>
+      new Promise((resolve, reject) => server.listen(port, err => (err ? reject(err) : resolve()))),
+    stop: () =>
+      new Promise((resolve, reject) => server.close(err => (err ? reject(err) : resolve()))),
   }
 }
